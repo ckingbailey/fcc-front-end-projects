6c0a32e64d590360d6251262c16fba83cb11616c
ckingbailey <ckingbailey@gmail.com>
2018-02-01 13:59:52 -0800

    serve staic html file

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 // dependencies
 const express = require('express')
 const app = express()
-const request = require('request');
+const path = require('path')
+const request = require('request')
 
 // when in development do as developers do
 if (process.env.NODE_ENV !== 'production') {
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 3001
 const WEATHER_KEY = process.env.WEATHER_KEY
 const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
+const pubPath = path.resolve(__dirname, 'public')
 app.use((req, res, next) => {
   // in development allow other origins
   const allowOrigin = process.env.NODE_ENV === 'production' ?
@@ -24,6 +26,8 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use(express.static(pubPath))
+
 app.post('/weather', (req, res) => {
   // probably get weather here instead of returning key to front-end
   // parse req for units, lat, lon
@@ -33,7 +37,6 @@ app.post('/weather', (req, res) => {
   res.set('Content-Type', 'text/plain').send(WEATHER_KEY)
 })
 
-const listener = app.listen(PORT, (req, res) => {
-  const listeningOn = 'Listening on port ' + listener.address().port
-  console.log(listeningOn)
+const listener = app.listen(PORT, () => {
+  console.log('Listening on port ' + listener.address().port)
 })
