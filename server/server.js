915c5605da348640124c30a5071063acd2f431c1
ckingbailey <ckingbailey@gmail.com>
2018-01-31 19:38:11 -0800

    rm key from weather app & allow origin * in dev

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 // dependencies
 const express = require('express')
 const app = express()
+const request = require('request');
 
 // when in development do as developers do
 if (process.env.NODE_ENV !== 'production') {
@@ -12,16 +13,23 @@ if (process.env.NODE_ENV !== 'production') {
 // config
 const PORT = process.env.PORT || 3001
 const WEATHER_KEY = process.env.WEATHER_KEY
+const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://ckingbailey.com')
+  // in development allow other origins
+  const allowOrigin = process.env.NODE_ENV === 'production' ?
+    'http://ckingbailey.com' : '*'
+  res.setHeader('Access-Control-Allow-Origin', allowOrigin)
   res.setHeader('Vary', 'Origin')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
-app.get('/weather', (req, res) => {
+app.post('/weather', (req, res) => {
   // probably get weather here instead of returning key to front-end
   // parse req for units, lat, lon
+  const units = 'imperial'
+  const latlon = `lat=${req.body.lat}&lon=${req.body.lon}`
+  const target = `${WEATHER_URL}?APPID=${WEATHER_KEY}&${latlon}&units=imperial`
   res.set('Content-Type', 'text/plain').send(WEATHER_KEY)
 })
 
