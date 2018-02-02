c9ab315e8e031e8e79d194b141b08be3027b25d2
ckingbailey <ckingbailey@gmail.com>
2018-02-02 14:12:16 -0800

    fix: cleanup imports in index.js

--- weather/js/dev/index.js
+++ weather/js/dev/index.js
@@ -1,16 +1,5 @@
-import { randomInt, randomFlt } from './utils/randomRolls';
-import clear from './weather/clear';
-import clouds from './weather/clouds';
-import drizzle from './weather/drizzle';
-import extreme from './weather/extreme';
-import fog from './weather/fog';
-import rain from './weather/rain';
-import snow from './weather/snow';
-import thunder from './weather/thunder';
-import tornado from './weather/tornado';
 import animateWeather from './weather/animateWeather';
-import {temperature, getWeather, getLocation} from './ajax/getWeather';
-import convert, {unitsBtn} from './utils/conversion';
+import { getWeather, getLocation } from './ajax/getWeather';
 
 getLocation('http://ip-api.com/json', function(location) {
   getWeather(location, function(weatherId) {
