e5dc35100583b061236eb56874955c5ab481d393
ckingbailey <ckingbailey@gmail.com>
2018-02-01 16:25:30 -0800

    successfull return openweatherapi response

--- server.js
+++ server.js
@@ -30,18 +30,27 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(bodyParser, { extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 app.post('/weather', (req, res) => {
   // probably get weather here instead of returning key to front-end
   // parse req for units, lat, lon
-  const body = multer(req.body)
-  console.log(body)
-  // const units = 'imperial'
-  // const latlon = `lat=${req.body.lat}&lon=${req.body.lon}`
-  // const target = `${WEATHER_URL}?APPID=${WEATHER_KEY}&${latlon}&units=imperial`
-  res.set('Content-Type', 'text/plain').send(WEATHER_URL)
+  if (!req.body) return res.status(400).send('We need your POST data, bub')
+  console.log('incoming request body:', req.body)
+  const units = 'imperial'
+  const latlon = `lat=${req.body.lat}&lon=${req.body.lon}`
+  const target = `${WEATHER_URL}?APPID=${WEATHER_KEY}&${latlon}&units=imperial`
+  request(target, (err, response, body) => {
+    // I'm recycling the arg name `res` is that bad?
+    if (err) res.status(500).send('There was a problem with the request')
+    else if (!body) res.status(500).send('Where\'s the body?')
+    else if (body) {
+      console.log('openweatherapi response:', JSON.parse(body))
+      res.set('Content-Type', 'application/json')
+      res.send(JSON.parse(body))
+    }
+  })
 })
 
 // app.use(express.static(pubPath))
