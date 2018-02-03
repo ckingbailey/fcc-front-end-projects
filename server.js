b7f9ce7afa72ccd7839536447a748fab418dd1c2
ckingbailey <ckingbailey@gmail.com>
2018-02-02 17:18:13 -0800

    de-line server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // dependencies
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const request = require('request')
 
 const app = express()
@@ -17,19 +17,19 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 3001
 const WEATHER_KEY = process.env.WEATHER_KEY
 const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
-const pubPath = path.resolve(__dirname, 'public')
+// const pubPath = path.resolve(__dirname, 'public')
 
 app.use((req, res, next) => {
   // in development allow other origins
   const allowOrigin = '*'
-  console.log('Access-Control-Allow-Origin', allowOrigin);
+  console.log('Access-Control-Allow-Origin', allowOrigin)
   res.setHeader('Access-Control-Allow-Origin', allowOrigin)
   res.setHeader('Vary', 'Origin')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // routes
 app.post('/weather', (req, res) => {
@@ -37,9 +37,9 @@ app.post('/weather', (req, res) => {
   // parse req for units, lat, lon
   if (!req.body) return res.status(400).send('We need your POST data, bub')
   console.log('incoming request body:', req.body)
-  const units = 'imperial'
+  const units = 'units=imperial'
   const latlon = `lat=${req.body.lat}&lon=${req.body.lon}`
-  const target = `${WEATHER_URL}?APPID=${WEATHER_KEY}&${latlon}&units=imperial`
+  const target = `${WEATHER_URL}?APPID=${WEATHER_KEY}&${latlon}&${units}`
   request(target, (err, response, body) => {
     // I'm recycling the arg name `res` is that bad?
     if (err) res.status(500).send('There was a problem with the request')
